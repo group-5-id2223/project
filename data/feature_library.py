f395c4cd05686532abf7e35b49d7e9cee4efdb56
evilmonkey19 <eperpinyapitarch@gmail.com>
2024-01-02 18:09:30 +0100

    Changed the feature pipelines

--- data/feature_library.py
+++ data/feature_library.py
@@ -3,19 +3,31 @@ import requests, json
 BASE_URL = "https://hacker-news.firebaseio.com/v0"
 
 def get_last_ten_stories() -> json:
-    stories_ids: list[int] = requests.get(f"{BASE_URL}/topstories.json").json()[:10]
-    stories: list = [get_story_by_id(id) for id in stories_ids]
+    stories: list = []
+    stories_ids: list[int] = requests.get(f"{BASE_URL}/topstories.json").json()
+    i = 0
+    for story_id in stories_ids:
+        if is_story(get_item_by_id(story_id)):
+            stories.append(get_item_by_id(story_id))
+        if len(stories) >= 10:
+            break
     return stories
 
+def get_max_item() -> int:
+    return requests.get(f"{BASE_URL}/maxitem.json").json()
 
-def get_story_by_id(id: str) -> json:
+def get_item_by_id(id: str) -> json:
     return requests.get(f"{BASE_URL}/item/{id}.json").json()
+
+def is_story(item: json) -> bool:
+    return item['type'] == 'story'
 
 def get_user_by_id(id: str) -> json:
     return requests.get(f"{BASE_URL}/user/{id}.json").json()
 
-def parse_story(story: json) -> json:
+def process_story(story: json) -> json:
     user = get_user_by_id(story['by'])
+    print(story)
     return {
         'id': story['id'],
         'title': story['title'],
@@ -26,9 +38,3 @@ def parse_story(story: json) -> json:
         'by': story['by'],
         'karma': user['karma']
     }
-
-
-if __name__ == '__main__':
-    stories = get_last_ten_stories()
-    data: list = [parse_story(story) for story in stories]
-    print(data)
---
+++ data/feature_pipeline_backfill.py
@@ -0,0 +1,75 @@
+import csv, requests, json, concurrent.futures, argparse, random
+from datetime import datetime
+import feature_library as fl
+
+FOLDER_PATH = '.'
+
+def process_item(i: int) -> json:
+    try:
+        item: json = fl.get_item_by_id(i)
+        if fl.is_story(item):
+            return fl.process_story(item)
+        return None
+    except Exception as e:
+        return None
+
+def write_items_to_tsv(items: list[dict], file_name: str):
+    with open(file_name, 'at') as out_file:
+        tsv_writer = csv.writer(out_file, delimiter='\t')
+        tsv_writer.writerow(items[0].keys())
+        for item in items:
+            tsv_writer.writerow(item)
+
+def format_number(number: int) -> str:
+    if number >= 1_000_000_000:
+        return f"{number / 1_000_000_000:.1f}B"
+    if number >= 1_000_000:
+        return f"{number / 1_000_000:.1f}M"
+    if number >= 1_000:
+        return f"{number / 1_000:.1f}K"
+    return str(number)
+
+def generate_unique_random(min_val: int, max_val: int, existing_set: set) -> int:
+    while True:
+        rand_num = random.randint(min_val, max_val)
+        if rand_num not in existing_set:
+            return rand_num
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--rows', metavar='N', type=int, help='an integer for the number of rows',)
+    parser.add_argument('--batch_size', metavar='N', type=int, help='an integer for the number of rows',
+                        default=100)
+
+
+
+    args = parser.parse_args()
+    max_rows: int = fl.get_max_item()
+    n_rows: int = args.rows if args.rows is not None else max_rows
+    batch_size: int = args.batch_size
+
+    now = datetime.now()
+    formatted_time = now.strftime("%Y-%m-%d-%H-%M")
+    base_file_name = f'{FOLDER_PATH}/data-{formatted_time}.tsv'
+    max_item: int = fl.get_max_item()
+    stories: list[dict] = []
+    numbers: set[int] = set()
+    written_items: int = 0
+    
+    while written_items < n_rows:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            while len(stories) < batch_size:
+                if written_items >= n_rows:
+                    break
+                number = generate_unique_random(1, max_item, numbers)
+                numbers.add(number)
+                item = executor.submit(process_item, number)
+                if item.result() is not None:
+                    stories.append(item.result())
+        written_items += batch_size
+        write_items_to_tsv(stories, base_file_name)
+        stories.clear()
+        print(f"Processed {written_items} of {n_rows} - {datetime.now()}")
+    print(f"Done. Time taken: {datetime.now() - now}")
